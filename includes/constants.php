f34c143969496e5455780174c3cfcea5e5ccc4b8
Eugene <eugenemikhai@gmail.com>
2018-10-11 13:16:16 -0400

    new migration

--- includes/constants.php
+++ includes/constants.php
@@ -3,6 +3,6 @@
 define('DB_SERVER', 'localhost');
 define('DB_SERVER_USERNAME', 'root');
 define('DB_SERVER_PASSWORD', '123pss');
-define('DB_DATABASE', 'erp2');
+define('DB_DATABASE', 'giftingGuruDB');
 
 ?>
