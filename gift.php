644c7c79e0420bc1e766c8327c6f63bc4ae042c6
Akash Chhabria <akashrai10.ar10@gmail.com>
2018-11-04 23:23:09 -0500

    Add files via upload

--- gift.php
+++ gift.php
@@ -17,7 +17,6 @@
     <!-- bootstarp js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
     <!--change script-->
     <script type="text/javascript" src="selectScript.js"></script>
 
@@ -27,31 +26,10 @@
 </head>
 <body>
 
-<div style="height: 100vh;" class="pimg6">
+<div style="height: 100%;" class="pimg6">
 
-    <!-- navbar -->
-    <!--<nav style="opacity: 0.8; width: 100%;" class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="./index.html"><img src="./img/gifting_guru_logo.webp"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="./index.html">HOME<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./index.html#about_us">ABOUT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./index.html#contact_us">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-    </nav>-->
-
-    <nav style="/*opacity: 0.8;*/ background: white; width: 100%;" class="navbar navbar-expand-lg navbar-light row">
+    <nav style="background: white; width: 100%;" class="navbar navbar-expand-lg navbar-light row">
         <a class="navbar-brand" href="#"><img src="./img/gifting_guru_logo.webp"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -60,13 +38,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li style="margin: 0 2rem;" class="nav-item">
-                    <a class="nav-link" href="./index.html">GIFT <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="./index.php">GIFT <span class="sr-only">(current)</span></a>
                 </li>
                 <li style="margin: 0 2rem;" class="nav-item">
-                    <a class="nav-link" href="./index.html#about_us">ABOUT</a>
+                    <a class="nav-link" href="./index.php#about_us">ABOUT</a>
                 </li>
                 <li style="margin: 0 2rem;" class="nav-item">
-                    <a class="nav-link" href="./index.html#contact_us">CONTACT</a>
+                    <a class="nav-link" href="./index.php#contact_us">CONTACT</a>
                 </li>
             </ul>
         </div>
@@ -82,27 +60,27 @@
         <div style="width: 100%; margin:0;"  class="row">
             <!-- image 1 -->
             <div style="margin: 5%;" class="spacing-top-bottom col">
-                <button style="padding: 4rem; width:100%; " class="animate_btn_on_hover border trans how_it_works">
+                <button style="padding: 4rem; width:80%; display: grid;" class="animate_btn_on_hover border trans how_it_works">
                     <img class="blur_img" style="width: 100%; height: 100%" src="./img/gifting_imgs/gift_page_s1.png">
                     <p class="show_p_on_hover">FILL OUT A BRIEF PERSONA ABOUT THE PERSON YOU ARE GIFTING TO</p>
                 </button>
-                <label style="padding: 2% 2% 2% 2%; margin: 0 auto; font-size: 26px;">Fill</label>
+                <label style="padding: 2% 22% 2% 2%; margin: 0 auto; font-size: 26px;">Fill</label>
             </div>
             <!-- image 2 -->
             <div style="margin: 5%;" class="spacing-top-bottom col">
-                <button style="padding: 4rem; margin-top: 0;" class="animate_btn_on_hover border trans how_it_works">
+                <button style="padding: 4rem; width:80%; display: grid;" class="animate_btn_on_hover border trans how_it_works">
                     <img class="blur_img" style="width: 100%; height: 100%" src="./img/gifting_imgs/gift_page_s2.png">
                     <p class="show_p_on_hover">SUBMIT IT FOR EVALUATION</p>
                 </button>
-                <label style="padding: 2% 2% 2% 2%; margin: 0 auto; font-size: 26px;">Submit</label>
+                <label style="padding: 2% 22% 2% 2%; margin: 0 auto; font-size: 26px;">Submit</label>
             </div>
             <!-- image 3 -->
             <div style="margin: 5%;" class="spacing-top-bottom col">
-                <button style="padding: 4rem; margin-top: 0;" class="animate_btn_on_hover border trans how_it_works">
+                <button style="padding: 4rem;width:80%; display: grid;" class="animate_btn_on_hover border trans how_it_works">
                     <img class="blur_img" style="width: 100%; height: 100%" src="./img/gifting_imgs/gift_page_s3.png">
                     <p class="show_p_on_hover">RECEIVE PERFECTLY CURATED GIFT RECOMMENDATIONS, INSTANTLY!</p>
                 </button>
-                <label style="padding: 2% 2% 2% 2%; margin: 0 auto; font-size: 26px;">Gift</label>
+                <label style="padding: 2% 22% 2% 2%; margin: 0 auto; font-size: 26px;">Gift</label>
             </div>
         </div>
 
@@ -116,6 +94,15 @@
         </div>
 
         <form action="showGift.php" method="post">
+            <div class="row text-justified">
+                <div class="col-3"></div>
+                <div class="col-6 form-group text-justified emailArea">
+                    <label for="email">What is your email?</label>
+                    <input type="text" class="form-control" id="email" placeholder="someone@example.com">
+                </div>
+                <div class="col-3"></div>
+            </div>
+
             <div class="row text-justified">
                 <div class="col-3"></div>
                 <div class="col-6 form-group text-justified ageArea">
@@ -156,9 +143,9 @@
 
             <div class="row text-justified">
                 <div class="col-3"></div>
-                <div class="col-6 form-group text-justified attrArea_1">
+                <div class="col-3 form-group text-justified attrArea_1">
                     <label for="Attribute_1">Pick the first attribute from below to describe your giftee</label> <br>
-                    <select class="form-control attr1" name="attributeGroup"  required>
+                    <select class="form-control attr1" name="attributeGroup" required>
                         <option value="Select">--Select an Item--</option>
                         <option value="Creative">Creative</option>
                         <option value="Adventurous">Adventurous</option>
@@ -169,12 +156,7 @@
                         <option value="Philosophical">Philosophical</option>
                     </select>
                 </div>
-                <div class="col-3"></div>
-            </div>
-
-            <div class="row text-justified">
-                <div class="col-3"></div>
-                <div class="col-6 form-group text-justified subattrArea_1">
+                <div class="col-3 form-group text-justified subattrArea_1">
                     <label for="subAttribute_1">Pick one sub attribute from below to describe your giftee</label> <br>
                     <select class="form-control sattr1" name="subattributeGroup" required>
                         <option value="">--Select an Item--</option>
@@ -185,7 +167,7 @@
 
             <div class="row text-justified">
                 <div class="col-3"></div>
-                <div class="col-6 form-group text-justified attrArea_2">
+                <div class="col-3 form-group text-justified attrArea_2">
                     <label for="Attribute_2">Pick the second attribute from below to describe your giftee</label> <br>
                     <select class="form-control attr2" name="attributeGroup2" required>
                         <option value="Select">--Select an Item--</option>
@@ -198,12 +180,7 @@
                         <option value="Philosophical">Philosophical</option>
                     </select>
                 </div>
-                <div class="col-3"></div>
-            </div>
-
-            <div class="row text-justified">
-                <div class="col-3"></div>
-                <div class="col-6 form-group text-justified subattrArea_2">
+                <div class="col-3 form-group text-justified subattrArea_2">
                     <label for="subAttribute_2">Pick one sub attribute from below to describe your giftee</label> <br>
                     <select class="form-control sattr2" name="subattributeGroup2" required>
                         <option value="">--Select an Item--</option>
@@ -214,8 +191,8 @@
 
             <div class="row text-justified">
                 <div class="col-3"></div>
-                <div class="col-6 form-group text-justified attrArea_2">
-                    <label for="Attribute_3">Pick the second attribute from below to describe your giftee</label> <br>
+                <div class="col-3 form-group text-justified attrArea_3">
+                    <label for="Attribute_3">Pick the third attribute from below to describe your giftee</label> <br>
                     <select class="form-control attr3" name="attributeGroup3" required>
                         <option value="Select">--Select an Item--</option>
                         <option value="Creative">Creative</option>
@@ -227,11 +204,7 @@
                         <option value="Philosophical">Philosophical</option>
                     </select>
                 </div>
-                <div class="col-3"></div>
-            </div>
-            <div class="row text-justified">
-                <div class="col-3"></div>
-                <div class="col-6 form-group text-justified subattrArea_3">
+                <div class="col-3 form-group text-justified subattrArea_3">
                     <label for="subAttribute_3">Pick one sub attribute from below to describe your giftee</label> <br>
                     <select class="form-control sattr3" name="subattributeGroup3" required>
                         <option value="">--Select an Item--</option>
@@ -239,10 +212,11 @@
                 </div>
                 <div class="col-3"></div>
             </div>
+
             <div class="row text-center" style="padding:3% 0 2% 0;">
                 <div class="col-2"></div>
                 <div class="col-8  text-center">
-                    <input class="submit gift_button border" name="submit" type="submit" value="Find me a Gift" style="color: white;">
+                    <button class="submit gift_button border" name="submit" type="submit" value="submit" style="color: white;">Find me a Gift</button>
                 </div>
                 <div class="col-2"></div>
             </div>
@@ -251,11 +225,13 @@
     </div>
 
 
-    <footer style="margin: 0; bottom: 0; position: relative; opacity: 0.9; background:white; padding:2% 1% 2% 0;" class="section-light page-footer col-12">
-
+    <!-- footer -->
+    <footer style="position: relative; opacity: 0.9; background:#eeeded; padding:1% 1% 1% 0;" class="section-light page-footer">
+        <hr class="style">
+        <br>
         <!-- logo for facebook and instagram -->
-        <div style="margin: 0 auto;" class="row">
-            <ul class="col inline_ul" style="margin: 0 auto; padding: 0 4% 1% 0;" >
+        <div style="margin: 0 auto; " class="row">
+            <ul class="col inline_ul" style="padding:0 2.5% 0 0;">
                 <li class="inline_li_margin">
                     <a href="https://www.facebook.com/giftingguru.ca" target="_blank"><img class="footer_logo" src="./img/facebook-logo.png" alt="facebook"></a>
                 </li>
@@ -264,13 +240,13 @@
                 </li>
             </ul>
         </div>
-        <div style="margin:0 auto; text-align: center;" class="row">
+        <div style="margin:0 auto; text-align: center; font-size:12px;" class="row">
             <p style="text-align: center;" class="col">
                 "Exchange gifts, as that will lead to increasing your love for one another" [Bukhari]
             </p>
         </div>
         <!-- Copyright -->
-        <div style="margin:0 auto; text-align: center;" class="footer-copyright">
+        <div style="margin:0 auto; text-align: center; font-size:10px;">
             &copy; 2018 by Gifting Guru
         </div>
     </footer>
